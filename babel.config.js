d99aa17d20978cea4aaac3c0383427fab0203b48
phongvanngo <19520834@gm.uit.edu.vn>
2022-04-15 10:52:28 +0700

    setup redux

--- babel.config.js
+++ babel.config.js
@@ -12,7 +12,9 @@ module.exports = function (api) {
 						interfaces: "./interfaces",
 						constants: "./constants",
 						hooks: "./hooks",
-						navigation: "./navigation"
+						navigation: "./navigation",
+						redux:"./redux"
+						
 					},
 				},
 			],
