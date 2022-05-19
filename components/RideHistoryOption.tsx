18773b96d97e078eb41da426da9760577021425c
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-05-19 23:52:01 +0700

    trang chuyen di

---
+++ components/RideHistoryOption.tsx
@@ -0,0 +1,70 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { placeIcon } from "assets/images";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { FixMeLater } from "interfaces/migration";
+
+const RideHistoryOption = ({ selectHandler }: FixMeLater) => {
+	const onSelectItem = () => {
+		if (selectHandler) {
+			selectHandler();
+		}
+	};
+
+	return (
+		<View style={tw`px-4 pt-1`}>
+			<TouchableOpacity
+				style={tw` items-center justify-between mb-2 px-4 shadow-md rounded-md flex-row py-2 bg-white`}
+				onPress={onSelectItem}
+			>
+				<View>
+					<View style={tw`flex-row items-center mb-1 `}>
+						<Text style={tw`font-bold`}>Điểm đi: </Text>
+						<Text>#nguyenkiet</Text>
+					</View>
+					<View style={tw`flex-row items-center mb-1 `}>
+						<Text style={tw`font-bold`}>Điểm đến: </Text>
+						<Text>7:00 ngày 29 tháng 9</Text>
+					</View>
+
+					<View style={tw`flex-row items-center mb-2`}>
+						<Text style={tw`font-bold`}>Khởi hành lúc: </Text>
+						<Text>Nguyễn Anh Kiệt</Text>
+					</View>
+
+					<View style={tw`flex-row items-center mb-2`}>
+						<Text></Text>
+					</View>
+				</View>
+				<View>
+					<Ionicons
+						name="caret-forward-outline"
+						size={22}
+						style={tw`opacity-80`}
+						color="#29282F"
+					/>
+				</View>
+				{/* <View
+					style={tw`flex-row items-end mb-1 bg-[#BDE952] px-2 rounded-full absolute right-4 bottom-2`}
+				>
+					<Text>Đã kết nối</Text>
+				</View> */}
+				{/* 
+				<View
+					style={tw`flex-row items-end mb-1 bg-[#F1F673] px-2 rounded-full absolute right-4 bottom-2`}
+				>
+					<Text>Đang chờ tài xế</Text>
+				</View> */}
+
+				<View
+					style={tw`flex-row items-end mb-1 bg-[#7EBC36] px-2 rounded-full absolute right-4 bottom-2`}
+				>
+					<Text>Đã hoàn thành</Text>
+				</View>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+export default RideHistoryOption;
--- screens/rides/Rides.tsx
+++ screens/rides/Rides.tsx
@@ -1,35 +1,72 @@
 import EditScreenInfo from "components/EditScreenInfo";
 import { Text, View } from "components/Themed";
-import { StyleSheet } from "react-native";
+import {
+	FlatList,
+	ScrollView,
+	StyleSheet,
+	TouchableOpacity,
+} from "react-native";
 import { RootTabScreenProps } from "types";
+import tw from "twrnc";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import SwitchSelector from "react-native-switch-selector";
+import { FixMeLater } from "interfaces/migration";
+import PlaceOption from "components/PlaceOption";
+import RideOption from "components/RideOption";
+import RideHistoryOption from "components/RideHistoryOption";
 
 export default function Rides({ navigation }: RootTabScreenProps<"Rides">) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Rides</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+	const options = [
+		{
+			label: "Đi nhờ xe",
+			value: "1",
+			testID: "switch-one",
+			accessibilityLabel: "switch-one",
+		},
+		{
+			label: "Tìm yên sau",
+			value: "2",
+			testID: "switch-one",
+			accessibilityLabel: "switch-one",
+		},
+	];
+
+	return (
+		<SafeAreaView style={tw`flex-1 bg-white px-6`}>
+			<View style={tw`flex-row justify-between h-1/10 items-center `}>
+				<TouchableOpacity>
+					<Ionicons name="caret-back" size={26} color="#7EBC36" />
+				</TouchableOpacity>
+
+				<Text style={tw`text-2xl font-bold`}>Lịch sử chuyến đi</Text>
+
+				<TouchableOpacity>
+					<Ionicons name="add-circle" size={26} color="#7EBC36" />
+				</TouchableOpacity>
+			</View>
+
+			<ScrollView>
+				<RideHistoryOption />
+				<RideHistoryOption />
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
 });
