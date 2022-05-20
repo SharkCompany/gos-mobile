432e176ca136560447b22d81bdfc7054c47ed15a
phongvanngo <19520834@gm.uit.edu.vn>
2022-05-20 08:09:38 +0700

    personal info

--- screens/personal/Personal.tsx
+++ screens/personal/Personal.tsx
@@ -1,9 +1,14 @@
+import { useNavigation } from "@react-navigation/native";
+import { userApi } from "app/api/user.api";
 import { useAppSelector } from "app/redux/store";
 import { selectUser } from "app/redux/user/userSlice";
-import { Text, TextTW, View } from "components/Themed";
+import MainButton from "components/MainButton";
+import { Text, TextTW, View, ViewTW } from "components/Themed";
 import * as ImagePicker from "expo-image-picker";
-import React, { useState } from "react";
-import { StyleSheet } from "react-native";
+import { FixMeLater } from "interfaces/migration";
+import { UserModel } from "models/User.model";
+import { default as React, useState } from "react";
+import { ScrollView, StyleSheet, TextInput } from "react-native";
 import { Avatar } from "react-native-elements";
 import tw from "twrnc";
 import { RootTabScreenProps } from "types";
@@ -11,6 +16,19 @@ import { RootTabScreenProps } from "types";
 export default function Personal({
   navigation,
 }: RootTabScreenProps<"Personal">) {
+  const userInfo = useAppSelector(selectUser);
+  console.log("user ne", userInfo);
+  const navigator = useNavigation();
+  const s = require("../../globalStyles");
+  const widthRelative = "80%";
+
+  const [phonenumber, setPhonenumber] = useState(userInfo?.phone_number);
+  const [bienSoXe, setBienSoXe] = useState(userInfo?.licensePlates);
+  const [bio, setBio] = useState(userInfo?.bio);
+  const [name, setName] = useState(userInfo?.name);
+
+  const [image, setImage] = useState<FixMeLater>(userInfo?.picture);
+
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -24,8 +42,24 @@ export default function Personal({
       setImage(result.uri);
     }
   };
-  const userInfo = useAppSelector(selectUser);
-  const [image, setImage] = useState(userInfo?.picture);
+
+  const handleSaveInformation = async () => {
+    const updateData: Partial<UserModel> = {
+      phone_number: phonenumber,
+      bio: bio,
+      licensePlates: bienSoXe,
+      picture: image,
+    };
+
+    try {
+      const data = await userApi.updateUser(updateData);
+      console.log("date login response", data);
+      navigator.navigate("Root");
+    } catch (error) {
+      console.log(error);
+    }
+    navigator.navigate("Root");
+  };
   return (
     <View style={tw`flex-1 items-center`}>
       <View style={tw`h-[50%] bg-[#7EBC36] w-full pt-10 px-6 rounded-b-3xl`}>
@@ -65,6 +99,64 @@ export default function Personal({
           )}
         </View>
       </View>
+      <ScrollView
+        style={{
+          width: "100%",
+          flex: 1,
+          paddingHorizontal: "6%",
+        }}
+      >
+        <View
+          style={{
+            borderBottomColor: "#E5E5E5",
+            borderBottomWidth: 1,
+            marginBottom: 20,
+          }}
+        />
+
+        <View style={styles.inputPairContainer}>
+          <Text style={styles.labelInput}>Tên của bạn</Text>
+          <TextInput
+            style={[styles.inputBasic, s.dropShadowButton]}
+            value={name}
+            onChangeText={(text) => setName(text)}
+            placeholder=""
+          />
+        </View>
+
+        <View style={styles.inputPairContainer}>
+          <Text style={styles.labelInput}>Số điện thoại</Text>
+          <TextInput
+            style={[styles.inputBasic, s.dropShadowButton]}
+            value={phonenumber}
+            onChangeText={(text) => setPhonenumber(text)}
+            placeholder="090743265"
+          />
+        </View>
+        <View style={styles.inputPairContainer}>
+          <Text style={styles.labelInput}>Biển số xe</Text>
+          <TextInput
+            style={[styles.inputBasic, s.dropShadowButton]}
+            value={bienSoXe}
+            onChangeText={(text) => setBienSoXe(text)}
+            placeholder="72E1-73202"
+          />
+        </View>
+        <View style={styles.inputPairContainer}>
+          <Text style={styles.labelInput}>Giới thiệu</Text>
+          <TextInput
+            multiline
+            numberOfLines={4}
+            scrollEnabled
+            style={[styles.inputMultiLine, s.dropShadowButton]}
+            value={bio}
+            onChangeText={(text) => setBio(text)}
+            placeholder="Hi tui la Kiet. Sinh vien nam 3 UIT"
+          />
+        </View>
+        <MainButton eventHandler={handleSaveInformation}>Cập nhật</MainButton>
+        <ViewTW className="h-10"></ViewTW>
+      </ScrollView>
     </View>
   );
 }
@@ -83,5 +175,36 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  headerH1: {
+    fontSize: 24,
+    marginBottom: 45,
+    fontWeight: "700",
+  },
+  inputBasic: {
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  inputMultiLine: {
+    padding: 10,
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    textAlignVertical: "top",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  labelInput: {
+    fontSize: 18,
+    paddingBottom: 3,
+    paddingLeft: 6,
+  },
+  inputPairContainer: {
+    flex: 1,
+    marginBottom: 24,
+    padding: 2,
+    paddingHorizontal: 10,
   },
 });
